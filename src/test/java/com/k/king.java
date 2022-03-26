ec0905830729aad9bcb1838552e5ea519bb8c695
mohanchennai <msundaram285@gmail.com>
2022-03-26 17:10:05 +0530

    sucess

---
+++ src/test/java/com/k/king.java
@@ -0,0 +1,31 @@
+package com.k;
+
+public class king {
+public static void main(String[] args) {
+		
+		
+		int a=120,b=230;
+		System.out.println("before swapping");
+		
+		System.out.println("value of a"+a);
+		System.out.println("value of b"+b);
+		
+		a=a+b;//120+230
+		b=a-b;//350-230
+		a=a-b;//350-120
+		
+		System.out.println("after swapping");
+		
+		System.out.println("value of a"+a);
+		System.out.println("value of b"+b);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
